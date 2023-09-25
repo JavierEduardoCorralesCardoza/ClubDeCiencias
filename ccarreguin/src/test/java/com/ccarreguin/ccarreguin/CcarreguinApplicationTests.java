a6d678ae7c14ce462b1e326cad995f51797c8bd0
Javier Eduardo Corrales Cardoza <ecardoza112@gmail.com>
2023-09-25 00:30:16 -0600

    Proyecto de react junt ocon spring

---
+++ ccarreguin/src/test/java/com/ccarreguin/ccarreguin/CcarreguinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarreguin.ccarreguin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcarreguinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
