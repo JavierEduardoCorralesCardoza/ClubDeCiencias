956ee9b24a896070ebc023c37dd4c1717f1ff1b1
Javier Eduardo Corrales Cardoza <ecardoza112@gmail.com>
2023-09-25 18:10:55 -0600

    Commit de prueba

---
+++ ccarreguin/src/main/java/com/ccarreguin/ccarreguin/controllers/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.ccarreguin.ccarreguin.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuarioController {
+    
+    @GetMapping
+    public String logIn(){
+        return "";
+    }
+}
